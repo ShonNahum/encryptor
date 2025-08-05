32d158ad68910796dc8db66f56c14ca699d4ce1e
ShonNahum <shon304082928@gmail.com>
2025-08-06 00:55:14 +0300

    noam cr -before

--- src/main/java/shon/encryptor/Main.java
+++ src/main/java/shon/encryptor/Main.java
@@ -1,4 +1,6 @@
 package shon.encryptor;
+// FUNCTION OR METHOD DONT DO MORE THEN WHAT SHE DID.. NOT EVEN PRINT OR STRING.
+// FUNCTION NEVER GETTING MORE THEN 3 PARAMETERS  IF ITS MORE THEN 3 MAKE OBJECT
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/shon/encryptor/utils/TimerHandler.java
+++ src/main/java/shon/encryptor/utils/TimerHandler.java
@@ -3,7 +3,7 @@ package shon.encryptor.utils;
 import shon.encryptor.interfaces.Timer;
 
 public class TimerHandler implements Timer { // overkill , its the only Timer no need interface for that
-    @Override
+    @Override // can be static BUT also can be object and instance for every class, and no need to get start and stop time in the class, just active the function
     public long start() {
         return System.nanoTime();
     }
@@ -14,7 +14,7 @@ public class TimerHandler implements Timer { // overkill , its the only Timer no
     }
 
     @Override
-    public double getDurationMillis(long startTime, long stopTime) {
+    public double getDurationMillis(long startTime, long stopTime)
         final double oneMillion = 1_000_000.0;
         return (stopTime - startTime) /  oneMillion;
     }
